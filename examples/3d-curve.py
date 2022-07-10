40d5f9a3fe885f7b506c29a4038f761e98db11e2
Dimitris Gahtidis <dimitrigahtidis@gmail.com>
2022-07-11 01:21:14 +1000

    The curve function gamma was wrong in the z-component (fixed). With regards to plotting the velocity vector of the particle: replaced quiver with the arrow3D class from the PYDrawing3D module. Changed some limits for the 3D plot and added save functionality so that the final animation is saved as a gif

--- examples/3d-curve.py
+++ examples/3d-curve.py
@@ -5,13 +5,14 @@ sys.path.append(os.path.abspath('.'))
 
 from modules.numbers import Dnum
 from modules.functions import *
+import modules.PYDrawing3D
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 def gamma(t):
-    return (sin(math.pi/5 * t),sin(math.pi/3 * t), t)
+    return (sin(math.pi/5 * t),sin(math.pi/3 * t), 5*t)
 
 steps = 100
 
@@ -40,20 +41,44 @@ for point in curve:
     dY.append(dy)
     dZ.append(dz)
 
+X = np.array(X)
+Y = np.array(Y)
+Z = np.array(Z)
+dX = np.array(dX)
+dY = np.array(dY)
+dZ = np.array(dZ)
+
+
 
 def animate_function(i):
+    x, y, z = X[i], Y[i], Z[i]
+    dx, dy, dz = dX[i], dY[i], dZ[i]
+    t = time[i].re
+
     ax.clear()
-    ax.plot3D(X, Y, Z, color="black")
-    ax.scatter(X[i], Y[i], Z[i], color="blue")
-    ax.quiver(X[i] , Y[i], Z[i], dX[i], dY[i], dZ[i], color="blue", normalize=True)
-    ax.set_ylim(-2, 2)
-    ax.set_xlim(-2, 2)
-    ax.set_title(f"Time = {np.round(time[i].re, decimals=2)} sec")
+    ax.plot3D(X[:i+1], Y[:i+1], Z[:i+1], color="black")
+    ax.scatter(x, y, z, color="blue")
+    ax.arrow3D(x , y, z, dx, dy, dz, mutation_scale=10, color="blue", arrowstyle="-|>")
+    ax.set_xlim3d([-1, 1])
+    ax.set_ylim3d([-1, 1])
+    ax.set_zlim3d([0, 100])
+    ax.set_title(f"Time = {np.round(t, decimals=2)} sec")
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
 
-
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-curve_ani = animation.FuncAnimation(fig, animate_function, interval=10, frames=steps)
+
+curve_ani = animation.FuncAnimation(fig, animate_function, interval=100, frames=steps)
+
 plt.show()
+
+# Saving the Animation
+folder_path = os.path.abspath(os.path.join('.','animations'))
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+file_path = os.path.join(folder_path, '3d-curve.gif')
+writergif = animation.PillowWriter(fps=steps/6)
+curve_ani.save(file_path, writer=writergif)
+
