a6c43c7fc14324338dadd657150a384bf603bc7f
Dimitris Gahtidis <dimitrigahtidis@gmail.com>
2022-07-18 20:20:01 +1000

    added numpy support for all functions

--- modules/functions.py
+++ modules/functions.py
@@ -1,36 +1,37 @@
 
 from modules.numbers import Dnum
 import math
+import numpy as np
 
 def sin(z):
-    return Dnum(math.sin(z.re), math.cos(z.re) * z.im)
+    return Dnum(np.sin(z.re), np.cos(z.re) * z.im)
 def cos(z):
-    return Dnum(math.cos(z.re), -math.sin(z.re) * z.im)
+    return Dnum(np.cos(z.re), -np.sin(z.re) * z.im)
 def tan(z):
-    return Dnum(math.tan(z.re), 1/(math.cos(z.re)**2) * z.im)
+    return Dnum(np.tan(z.re), 1/(np.cos(z.re)**2) * z.im)
 def exp(z):
-    return Dnum(math.exp(z.re), math.exp(z.re) * z.im)
+    return Dnum(np.exp(z.re), np.exp(z.re) * z.im)
 def log(z):
-    return Dnum(math.log(z.re), 1/(z.re) * z.im)
+    return Dnum(np.log(z.re), 1/(z.re) * z.im)
 def pow(z, n):
-    return Dnum(math.pow(z.re,n), n * math.pow(z.re, n-1) * z.im)
+    return Dnum(np.pow(z.re,n), n * np.pow(z.re, n-1) * z.im)
 def sqrt(z):
-    return Dnum(math.sqrt(z.re), (0.5) * math.sqrt(z.re) * z.im)
+    return Dnum(np.sqrt(z.re), (0.5) * np.sqrt(z.re) * z.im)
 def acos(z):
-    return Dnum(math.acos(z.re), (-1)/math.sqrt(1-z.re**2) * z.im)
+    return Dnum(np.acos(z.re), (-1)/np.sqrt(1-z.re**2) * z.im)
 def asin(z):
-    return Dnum(math.asin(z.re), 1/math.sqrt(1-z.re**2) * z.im)
+    return Dnum(np.asin(z.re), 1/np.sqrt(1-z.re**2) * z.im)
 def atan(z):
-    return Dnum(math.atan(z.re), 1/(1+z.re**2) * z.im)
+    return Dnum(np.atan(z.re), 1/(1+z.re**2) * z.im)
 def sinh(z):
-    return Dnum(math.sinh(z.re), math.cosh(z.re) * z.im)
+    return Dnum(np.sinh(z.re), np.cosh(z.re) * z.im)
 def cosh(z):
-    return Dnum(math.cosh(z.re), math.sinh(z.re) * z.im)
+    return Dnum(np.cosh(z.re), np.sinh(z.re) * z.im)
 def tanh(z):
-    return Dnum(math.tanh(z.re), (1 - math.tanh(z.re)**2) * z.im)
+    return Dnum(np.tanh(z.re), (1 - np.tanh(z.re)**2) * z.im)
 def asinh(z):
-    return Dnum(math.asinh(z.re), 1/(1-z.re**2) * z.im)
+    return Dnum(np.asinh(z.re), 1/(1-z.re**2) * z.im)
 def acosh(z):
-    return Dnum(math.acosh(z.re), 1/math.sqrt(z.re**2-1) * z.im)
+    return Dnum(np.acosh(z.re), 1/np.sqrt(z.re**2-1) * z.im)
 def erf(z):
-    return Dnum(math.erf(z.re), (2/math.sqrt(math.pi))*math.exp(-(z.re**2)) * z.im)
+    return Dnum(np.erf(z.re), (2/np.sqrt(np.pi))*np.exp(-(z.re**2)) * z.im)
