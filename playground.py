88fcf42f63cc27d91072fa53146bf7ede4623383
Dimitris Gahtidis <dimitrigahtidis@gmail.com>
2022-07-18 20:38:26 +1000

    Demonstrating that the Jacobian and directional derivatives of a function can be computer directly through dual numbers

--- playground.py
+++ playground.py
@@ -1,4 +1,3 @@
-#%%
 import sys
 import os
 
@@ -10,57 +9,24 @@ import modules.PYDrawing3D
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
+
+def sigma(z):
+    u, v = z
+    return Dnum.split((cos(u)*cos(v), cos(u)*sin(v), sin(u)))
 
 
-n_side_points = 50
 
-lattice = []
-for x in range(n_side_points):
-    for y in range(n_side_points):
-        lattice.append(Dnum(x/n_side_points*4-2,y/n_side_points*4-2))
+u = np.array([math.pi/4, math.pi/4])
+du = np.array([1, 0])
+z = Dnum(u,du)
+print('simga(u + du) = sigma(u) + Jacobian(sigma) * du i_0')
+print('for example if sigma parametrizes the sphere...')
+print(f'sigma({z}) = {sigma(z)}')
 
-steps = 200
-d_theta = 1/steps
-
-transformation = []
-X = []
-Y = []
-
-for i in range(steps):
-    transformation.append([point * exp(Dnum(0,d_theta*i)) for point in lattice])
-
-for lattice in transformation:
-    X.append([point.re for point in lattice])
-    Y.append([point.im for point in lattice])
-
-
-def animate_function(i):
-    x = X[i]
-    y = Y[i]
-
-    ax.clear()
-    ax.scatter(x, y, color="black")
-    ax.set_xlim([-1, 1])
-    ax.set_ylim([-1, 1])
-    ax.set_title(f'$e^{{{round(d_theta*i,2)}\\varepsilon}} (x + y \\varepsilon)$')
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    #ax.legend(loc="upper left", bbox_to_anchor=(1.05,1))
-    plt.tight_layout()
-
-fig = plt.figure()
-ax = plt.axes()
-
-curve_ani = animation.FuncAnimation(fig, animate_function, interval=100, frames=steps)
-
-plt.show()
-
-# Saving the Animation
-folder_path = os.path.abspath(os.path.join('.','animations'))
-if not os.path.exists(folder_path):
-    os.makedirs(folder_path)
-
-file_path = os.path.join(folder_path, 'playground.gif')
-writergif = animation.PillowWriter(fps=steps/10)
-curve_ani.save(file_path, writer=writergif)
+u = np.array([math.pi/4, math.pi/4])
+du = np.array([[1,0], [0,1]])
+z = Dnum(u,du)
+print()
+print('simga(u + I) = sigma(u) + Jacobian(sigma) * I i_0')
+print('for example if sigma parametrizes the sphere...')
+print(f'Jacobian(sigma) = \n{sigma(z).im}')
